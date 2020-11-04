0f65a61def01644f7cfa171ed9998b90fcd9121d
lelua <tauful01@myunitec.ac.nz>
2020-11-05 10:28:34 +1300

    I have created a Login

--- PHPandDatabase/login.php
+++ PHPandDatabase/login.php
@@ -6,4 +6,36 @@ $password = $_POST["password"];
 echo "username".$username;
 echo "password".$password;
 
+// Create a Database Connection:
+
+// Create a Database Connection
+$server = "nba02whlntki5w2p.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$dbusername = "k4j7jgjpv0fuxdca";
+$dbpassword = "ltbt9xigi8kadool";
+$dbname = "ntlwav83cf7kfzeb";
+
+$conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
+
+if ($conn->error){
+    echo $conn->error;
+}else{
+    echo "connected";
+}
+
+// Write a Query
+$sql = "select * from users
+where username = ".$username." and password = ".$password;
+
+
+// Execute The Query
+$result = mysqli_query($conn, $sql);
+
+if ($result->num_rows == 1){
+    echo "you have logged in";
+    while ($row = $result->fetch_assoc()) {
+        echo $row["id"];
+    }
+} else {
+    echo "wrong username or password";
+}
 
