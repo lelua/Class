afc52a5b1f2cb94371aa3056411c156f3fd9e83d
lelua <tauful01@myunitec.ac.nz>
2020-11-10 09:10:59 +1300

    php include

--- Include/index.php
+++ Include/index.php
@@ -5,7 +5,9 @@
     <title>Include Exercise</title>
 </head>
 <body>
-<div id="header">Menu, Login, Pictures</div>
+<div id="header">
+<?php include "page1.php"; ?>
+Login, Pictures</div>
 <div id="main">Contents</div>
 <div id="footer">Contact Information</div>
 </body>
