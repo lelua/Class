dcfbabacb67d5520f8c631767757dca331955fa5
lelua <tauful01@myunitec.ac.nz>
2020-11-10 09:07:10 +1300

    changed typo

--- Include/page1.php
+++ Include/page1.php
@@ -1,5 +1,5 @@
 <ul>
     <li><a href="index.php"></a>Home</li>
-    <li><a href="AboutUs.php"></a>About US</li>
+    <li><a href="AboutUs.php"></a>About Us</li>
     <li><a href="Products.php"></a>Products</li>
 </ul>
