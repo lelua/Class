86ea5d00445246223571cf5b396aed64fbf637a8
lelua <tauful01@myunitec.ac.nz>
2020-11-10 09:21:04 +1300

    changed href

--- Include/Menu.php
+++ Include/Menu.php
@@ -1,5 +1,5 @@
 <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="AboutUs.php">About Us></a></li>
-    <li><a href="Products.php">Products></a></li>
+    <li><a href="AboutUs.php">About Us</a></li>
+    <li><a href="Products.php">Products</a></li>
 </ul>
