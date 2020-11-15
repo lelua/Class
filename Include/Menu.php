c79a11fba33737c91f1fffeb82551ba0ca5003ad
lelua <tauful01@myunitec.ac.nz>
2020-11-16 09:36:45 +1300

    fixed spelling mistake for include php category

--- Include/Menu.php
+++ Include/Menu.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="index.php">Home</a></li>
-    <?php include "caregory.php";?>
+    <?php include "category.php";?>
     <li><a href="AboutUs.php">About Us</a></li>
-    <li><a href="Products.php">Products</a></li>
+    <li><a href="Products.php">Products</a></li>git
 </ul>
