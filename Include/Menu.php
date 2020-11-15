9e713aaa43ce4310b6cce3bb8e06be2caee2d60e
lelua <tauful01@myunitec.ac.nz>
2020-11-16 09:31:44 +1300

    show category name on menu

--- Include/Menu.php
+++ Include/Menu.php
@@ -1,5 +1,6 @@
 <ul>
     <li><a href="index.php">Home</a></li>
+    <?php include "caregory.php";?>
     <li><a href="AboutUs.php">About Us</a></li>
     <li><a href="Products.php">Products</a></li>
 </ul>
