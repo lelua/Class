c76faac5cb6ef66ea7ef1cd3dc9c46f4b71a3a8a
lelua <tauful01@myunitec.ac.nz>
2020-11-19 10:43:07 +1300

    fixing login

--- Include/Menu.php
+++ Include/Menu.php
@@ -3,7 +3,7 @@
     <?php include "category.php";?>
     <li><a href="AboutUs.php">About Us</a></li>
     <li><a href="cart.php">Cart</a></li>
-    <li>include "login.php";</li>
+    <?php include "login.php";?>
     <?php
     @session_start();
         if (isset($_SESSION["userID"])){
