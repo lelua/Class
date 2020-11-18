0cd37557d91ac11a482146af4f2c4d04411bfb55
lelua <tauful01@myunitec.ac.nz>
2020-11-19 09:42:04 +1300

    updating products page

--- Include/products.php
+++ Include/products.php
@@ -9,24 +9,31 @@ $dbname = "ntlwav83cf7kfzeb";
 
 $conn = new mysqli($server, $dbusername, $dbpassword, $dbname);
 
-//2. Create Query
-$sql = "select * from products";
+//2. create a query
+// take input from selected category;
+if (isset($_GET["category"])){
+    echo "<h1>".$_GET["category"]."</h1>";
+    $sql = "select * from products where category = ".$_GET["category"];
+}else{
+    $sql = "select * from products";
+}
 
-//3. Execute Query on the Connection
+//3. run the query on that connection
 $result = mysqli_query($conn,$sql);
 
-//4. Show Result (For This, Change the Names according to the myadmin Category Names and img src.)
+//4. show result
 while ($row = $result->fetch_assoc()){
     ?>
-   <div>
-    <p><?php echo $row["name"]; ?></p>
-    <p><?php echo $row["price"]; ?></p>
-    <p><img src="<?php echo $row["image"]; ?>"</p>
-    <form action="addtocart.php" method="post">
-        <input name="productID" value="<?php echo $row["id"]; ?>" type="hidden">
-        <input name="qty" type="number" placeholder="QTY" min="0">
-        <input type="submit" value="Add to cart">
-    </form>
+    <div>
+        <p><?php echo $row["name"]; ?></p>
+        <p><?php echo $row["price"]; ?></p>
+        <p><img src="<?php echo $row["image"]; ?>"</p>
+        <form action="addToCart.php" method="post">
+            <input name="productID" value="<?php echo $row["id"]; ?>" type="hidden">
+            <input name="qty" type="number" placeholder="QTY" min="0">
+            <input type="submit" value="Add to cart">
+        </form>
     </div>
-<?php
+    <?php
+}
 }
