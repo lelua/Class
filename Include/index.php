3f7bc8e9640306230d2050abbc2b1ad1276403e1
lelua <tauful01@myunitec.ac.nz>
2020-11-20 00:45:54 +1300

    fixing heading tabs css

--- Include/index.php
+++ Include/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <title>Include Exercise</title>
     <link rel="stylesheet" href="CSS/style.css"
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,600&display=swap" rel="stylesheet">
 </head>
 <body>
 
