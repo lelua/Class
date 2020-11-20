d28b7ba08fd36cbdf710100138cc625339b7e909
lelua <tauful01@myunitec.ac.nz>
2020-11-20 15:31:56 +1300

    designing categories css

--- Include/Menu.php
+++ Include/Menu.php
@@ -1,14 +1,16 @@
-<ul>
-    <li><a href="index.php">HOME</a></li>
-    <?php include "category.php";?>
-    <li><a href="AboutUs.php">ABOUT US</a></li>
-    <li><a href="cart.php">CART</a></li>
-    <?php
-    @session_start();
-        if (isset($_SESSION["userID"])){
-            ?>
-            <li><a href="viewOrder.php"My Orders</a></li>
-    <?php
-        }
-    ?>
-</ul>
+<div class="menulist">
+    <ul>
+        <li><a href="index.php">HOME</a></li>
+        <?php include "category.php";?>
+        <li><a href="AboutUs.php">ABOUT US</a></li>
+        <li><a href="cart.php">CART</a></li>
+        <?php
+        @session_start();
+            if (isset($_SESSION["userID"])){
+                ?>
+                <li><a href="viewOrder.php"My Orders</a></li>
+        <?php
+            }
+        ?>
+    </ul>
+</div>
