17dbba49d90a895c9d27fb409cec26222721a34d
lelua <tauful01@myunitec.ac.nz>
2020-11-20 21:43:44 +1300

    designing register and login

--- Include/Menu.php
+++ Include/Menu.php
@@ -5,7 +5,9 @@
         <li><a href="">ABOUT US</a></li>
         <li><a href="cart.php">CART</a></li>
         <li><a href="registerForm.html">REGISTER</a></li>
-        <?php include "login.php"; ?>
+            <div class="userpass">
+                <?php include "login.php"; ?>
+            </div>
         <?php
         @session_start();
             if (isset($_SESSION["userID"])){
