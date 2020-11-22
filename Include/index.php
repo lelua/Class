64232ec1a769744c13d5973ae398f318980f478e
lelua <tauful01@myunitec.ac.nz>
2020-11-22 22:25:15 +1300

    Designing Index with Image

--- Include/index.php
+++ Include/index.php
@@ -22,9 +22,10 @@
 </div>
 
 <div class="container">
+    <img src="Images/puppyweb.jpg">
     <h1>WELCOME TO PET SHOP NEW ZEALAND</h1>
     <p>One Stop Shop For All Your Pet Needs</p>
-    <?php include "puppy.php"; ?>
+
 </div>
 
 <div id="products">
