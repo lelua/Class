395d6f6af46e9c1d6695eacdd5d95461a7c643bd
lelua <tauful01@myunitec.ac.nz>
2020-11-23 00:44:45 +1300

    fixed title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Include Exercise</title>
+    <title>PetShop New Zealand</title>
     <link rel="stylesheet" href="CSS/style.css"
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;0,800;1,600&display=swap" rel="stylesheet">
 </head>
